ea6effeed318e7082d816e141c93280bdaf7937b
Satyan <msatyan@gmail.com>
2019-07-28 16:16:54 -0500

    doc

--- src/sample1/faceRecognition.ts
+++ src/sample1/faceRecognition.ts
@@ -1,5 +1,5 @@
 import * as faceapi from 'face-api.js';
-const path = require('path');
+import * as path from 'path';
 
 import { canvas, faceDetectionNet, faceDetectionOptions, saveFile } from './commons';
 

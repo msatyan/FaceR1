0b0454c3724b8b22f39877ef973154366fce2fda
Satyan <msatyan@gmail.com>
2019-07-31 00:00:36 -0500

    test img

--- src/sample1/faceCompare.ts
+++ src/sample1/faceCompare.ts
@@ -1,56 +1,79 @@
 import * as faceapi from 'face-api.js';
 import * as path from 'path';
 
-import { canvas, faceDetectionNet, saveFile } from './commons';
-
-const weights_path = path.join(__dirname, '../../weights');
-let REFERENCE_IMAGE = path.join(__dirname, '../images/bbt1.jpg')
-let QUERY_IMAGE = path.join(__dirname, '../images/bbt4.jpg');
-
-const img_test = "C:/x-test-img/t1/";
-REFERENCE_IMAGE = img_test + "r1.jpg";
-QUERY_IMAGE = img_test + "q3.jpg";
+import { canvas, faceDetectionNet } from './commons';
 
 
-async function CompareImg(rImg: string, qImg: string, CustomConfidenceThreshold = 0.45) {
-  let rc = { match: false, distance: 100 };
+class FaceCompare {
+  minConfidence: number;
+  weights_path: string;
+  fDetectionOptions: any;
+  IsWeightLoaded: boolean;
 
-  const fDetectionOptions = new faceapi.SsdMobilenetv1Options({ minConfidence: 0.5 })
-
-  await faceDetectionNet.loadFromDisk(weights_path);
-  await faceapi.nets.faceLandmark68Net.loadFromDisk(weights_path)
-  await faceapi.nets.faceRecognitionNet.loadFromDisk(weights_path)
-
-  const referenceImage = await canvas.loadImage(rImg)
-  const queryImage = await canvas.loadImage(qImg)
-
-  const resultsRef = await faceapi.detectAllFaces(referenceImage, fDetectionOptions)
-    .withFaceLandmarks()
-    .withFaceDescriptors()
-
-  const resultsQuery = await faceapi.detectAllFaces(queryImage, fDetectionOptions)
-    .withFaceLandmarks()
-    .withFaceDescriptors()
-
-  if (resultsRef.length === 1 &&
-    resultsQuery.length === 1) {
-    const faceMatcher = new faceapi.FaceMatcher(resultsRef)
-    const bestMatch = faceMatcher.findBestMatch(resultsQuery[0].descriptor)
-    // console.log(`bestMatch ${bestMatch}`);
-    if ( bestMatch.distance <= CustomConfidenceThreshold ) {
-      rc.match = true;
-      rc.distance = bestMatch.distance;
-    }
+  constructor(weights_path: string, minConfidence = 0.5) {
+    this.weights_path = weights_path;
+    this.minConfidence = minConfidence;
+    this.IsWeightLoaded = false;
+    this.fDetectionOptions = new faceapi.SsdMobilenetv1Options(
+      { minConfidence: minConfidence });
   }
 
-  return( rc );
+  async Compare(rImg: string, qImg: string, CustomConfidenceThreshold = 0.45) {
+    let rc = { match: false, distance: 100 };
 
+    if ( this.IsWeightLoaded == false )
+    {
+      await faceDetectionNet.loadFromDisk(this.weights_path);
+      await faceapi.nets.faceLandmark68Net.loadFromDisk(this.weights_path);
+      await faceapi.nets.faceRecognitionNet.loadFromDisk(this.weights_path);
+      this.IsWeightLoaded = true;
+    }
+    const referenceImage = await canvas.loadImage(rImg)
+    const queryImage = await canvas.loadImage(qImg)
+
+    const resultsRef = await faceapi.detectAllFaces(referenceImage, this.fDetectionOptions)
+      .withFaceLandmarks()
+      .withFaceDescriptors()
+
+    const resultsQuery = await faceapi.detectAllFaces(queryImage, this.fDetectionOptions)
+      .withFaceLandmarks()
+      .withFaceDescriptors()
+
+    if (resultsRef.length === 1 &&
+      resultsQuery.length === 1) {
+      const faceMatcher = new faceapi.FaceMatcher(resultsRef)
+      const bestMatch = faceMatcher.findBestMatch(resultsQuery[0].descriptor)
+      // console.log(`bestMatch ${bestMatch}`);
+      if (bestMatch.distance <= CustomConfidenceThreshold) {
+        rc.match = true;
+        rc.distance = bestMatch.distance;
+      }
+    }
+    return (rc);
+  }
 }
 
-async function RUN() {
-  const rc = await CompareImg(REFERENCE_IMAGE, QUERY_IMAGE);
-  console.log(`rc = ` + JSON.stringify(rc) );
+
+async function test1()
+{
+  const wt_path = path.join(__dirname, '../../weights');
+
+  const TestImgPath1 = path.join(__dirname, "../images/test1/" )
+
+  let faceCompare = new FaceCompare( wt_path );
+  let rc = await faceCompare.Compare( TestImgPath1+"einstein1.jpg", TestImgPath1 + "einstein2.jpg", 0.5 );
+  console.log( JSON.stringify(rc) );
+
+  rc = await faceCompare.Compare( TestImgPath1 + "obama1.jpg", TestImgPath1 + "obama2.jpg" );
+  console.log( JSON.stringify(rc) );
+
+  rc = await faceCompare.Compare( TestImgPath1 + "stevejobs1.jpg", TestImgPath1 + "stevejobs2.jpg" );
+  console.log( JSON.stringify(rc) );
+
+  rc = await faceCompare.Compare( TestImgPath1 + "BillGates1.jpg", TestImgPath1 + "BillGates2.jpg" );
+  console.log( JSON.stringify(rc) );
+
+  console.log( "Done !!!" );
 }
 
-RUN()
-console.log( " Don !!!" );
+test1();

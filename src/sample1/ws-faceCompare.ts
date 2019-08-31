38be6538c4cea08b3d64d451b5d544c25b9bf475
Satyan <msatyan@gmail.com>
2019-08-31 00:20:53 -0500

    ws

--- src/sample1/ws-faceCompare.ts
+++ src/sample1/ws-faceCompare.ts
@@ -5,8 +5,6 @@ import * as path from 'path';
 
 import { canvas, faceDetectionNet } from './commons';
 
-// import * as express from 'express';
-// import * as http from 'http';
 import * as WebSocket from 'ws';
 import * as uuid from "uuid";
 
@@ -28,7 +26,7 @@ class FaceCompare {
   }
 
   async Compare(rImg: string, qImg: string, CustomConfidenceThreshold = 0.45) {
-    let rc = { match: false, distance: 100 };
+    let rc = { match: false, confidence: 0 };
 
     if ( this.IsWeightLoaded == false )
     {
@@ -55,7 +53,7 @@ class FaceCompare {
       // console.log(`bestMatch ${bestMatch}`);
       if (bestMatch.distance <= CustomConfidenceThreshold) {
         rc.match = true;
-        rc.distance = bestMatch.distance;
+        rc.confidence = (1 - bestMatch.distance) * 100;
       }
     }
     return (rc);
@@ -63,20 +61,8 @@ class FaceCompare {
 }
 
 
-async function test1()
-{
-  const wt_path = path.join(__dirname, '../../weights');
-  const TestImgPath1 = path.join(__dirname, "../images/test1/" )
-  let faceCompare = new FaceCompare( wt_path );
 
-  //let rc = await faceCompare.Compare( ImgStr1, ImgStr2, 0.5 );
-  // console.log( "JustinTrudeau Img: " + JSON.stringify(rc) );
-  console.log( "Done !!!" );
-}
-
-// test1();
-
-//wss.on('connection', (ws: WebSocket) => {
+//////////////////////////////////////////////
 wss.on('connection', (ws: any) => {
   // ws has to of type any to assign a new element
   ws['id'] = uuid.v4();
@@ -85,29 +71,42 @@ wss.on('connection', (ws: any) => {
   let faceCompare = new FaceCompare( wt_path );
 
   // The incoming message
-  ws.on('message', async function incoming(message: string) {
-    let msg_in = JSON.parse( message);
-    console.log( `id=${msg_in.id} and msg=${msg_in.msg}`)
+  ws.on('message', async function incoming( message: string) {
+    let req : any;
+    let action = "";
+    let id = 0;
 
-    let acn = {
-      msg: "acknowledge ",
-      acnid: ws.id,
-      msgid: msg_in.id,
-      res: 'well done'
-    };
-
-    let isML = true;
-    if( isML ) {
-      let rc = await faceCompare.Compare( TestImgPath1 + "t1/JustinTrudeau-1.jpg", TestImgPath1 + "t1/JustinTrudeau-3.jpg" );
-      const s = JSON.stringify(rc);
-      console.log( "JustinTrudeau: " + s );
-      ws.send( s );
-    } else {
-      ws.send( JSON.stringify(acn) ); // Send an acknowledgement
+    try {
+      req = JSON.parse(message);
+      action = req.action;
+      id = req.id;
     }
+    catch(error) {
+    }
+
+    let res  = undefined;
+    if( action == 'Compare2' ) {
+      let rc = await faceCompare.Compare(
+        TestImgPath1 + "t1/JustinTrudeau-1.jpg",
+        TestImgPath1 + "t1/JustinTrudeau-3.jpg" );
+
+      res =  {
+        id: id,
+        action: action,
+        match: rc.match,
+        confidence: rc.confidence
+      };
+
+    } else {
+      res = {id: id, action: "unknown request"};
+    }
+    const s = JSON.stringify(res);
+    console.log( s );
+    ws.send( s );
+
   });
 
-  ws.send('Connection Success Message From Server to Client ' + ws.id );
+   ws.send( JSON.stringify( {id: ws.id, action: "connection",  msg: "Success"} ) );
 
 });
 
